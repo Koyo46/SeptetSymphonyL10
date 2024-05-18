e48c6911be16fae0601d59616606c5d3ccc4133d
reiken <ultee529@outlook.jp>
2024-05-18 17:07:43 +0900

    apiテスト

--- routes/api.php
+++ routes/api.php
@@ -29,4 +29,8 @@ Route::get('/game/{gameId}/myHandCards', function ($gameId) {
 Route::post('/game/{gameId}/start', function ($gameId) {
     return GameController::startGame($gameId);
 });
+Route::get('/hello', function () {
+    return response()->json(['message' => 'ハロー']);
+});
+
 
