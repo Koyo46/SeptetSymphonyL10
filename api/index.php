0f1b0255aea516fc1d7453ab7228133dca70379a
reiken <ultee529@outlook.jp>
2024-05-30 13:46:14 +0900

    api/index.phpの修正

--- api/index.php
+++ api/index.php
@@ -1,6 +1,21 @@
 <?php
-/**
-* Here is the serverless function entry
-* for deployment with Vercel.
-*/
-require __DIR__.'/../public/index.php';
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// APIルートのみをロードする
+$app->router->group(['prefix' => 'api'], function ($router) {
+    require __DIR__.'/../routes/api.php';
+});
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
